2f71c252e3335d94e844b4a6c9d1dc977d1a6119
Anders Andersson <trezker@gmail.com>
2014-03-04 10:44:01 +0100

    Some random words

--- source/app.d
+++ source/app.d
@@ -3,24 +3,49 @@ import std.socket;
 import std.regex;
 import std.algorithm;
 import std.string;
+import std.random;
+import std.conv;
+
+// /usr/share/dict/words
+
+dstring[] Load_dictionary() {
+	writeln("Loading dictionary from '/usr/share/dict/words'");
+	auto f = File("/usr/share/dict/words");
+	scope(exit) f.close();
+	dstring[] lines;
+
+	foreach (str; f.byLine) {
+		lines ~= to!dstring(str.idup);
+	}
+
+	return lines;
+}
+
+bool Privmsg(Socket s, string channel, string msg) {
+	return Send(s, ("PRIVMSG " ~ channel ~ " :" ~ msg).idup);
+}
 
 bool Send(Socket s, string msg) {
-	auto result = s.send(msg);
+	auto result = s.send((msg ~ "\r\n").idup);
 	writeln("<", msg);
 	return result != Socket.ERROR;
 }
 
 void main() {
+	dstring[] dict = Load_dictionary();
+	dchar[] currentword;
+	dchar[] shuffledword;
+	
 	Socket s = new TcpSocket();
 	s.connect(new InternetAddress("irc.freenode.net", 6667));
 
-	if(!Send(s, "NICK ragaman\r\n")) {
+	if(!Send(s, "NICK ragaman")) {
 		writeln("Failed send");
 	}
-	if(!Send(s, "USER ragaman 0 * :anagram bot\r\n")) {
+	if(!Send(s, "USER ragaman 0 * :anagram bot")) {
 		writeln("Failed send");
 	}
-	if(!Send(s, "JOIN ##anagram\r\n")) {
+	if(!Send(s, "JOIN ##anagram")) {
 		writeln("Failed send");
 	}
 
@@ -68,9 +93,18 @@ void main() {
 			if(command == "PRIVMSG") {
 				if(nick == "Trezker") {
 					if(message == "!quit") {
-						Send(s, ("PRIVMSG " ~ channel ~ " :Shutting down\r\n").idup);
+						Privmsg(s, channel.idup, "Shutting down");
 						break;
 					}
+				}
+				if(message == "!start") {
+					ulong number = uniform(0, dict.length);
+					currentword = dict[number].dup;
+					shuffledword = dict[number].dup;
+					randomShuffle(shuffledword);
+					Privmsg(s, channel.idup, ("Number: " ~ to!string(number)).idup);
+					Privmsg(s, channel.idup, ("Word: " ~ to!string(currentword)));
+					Privmsg(s, channel.idup, ("Word: " ~ to!string(shuffledword)));
 				}
 			}
 		}
